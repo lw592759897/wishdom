89396d9e2391653e5771c2ae84986bf16746b531
liujp2013 <liujianping@elinkchina.net>
2013-09-17 15:57:29 +0800

    刘建平，修改

---
+++ wishdom/WebContent/front/custmode.ftl
@@ -0,0 +1,29 @@
+<#include "/include/head.ftl">
+<!-- index-top over -->
+<title>智慧金-产品定制</title>
+<h3 class="h3-art-work"></h3>
+<div class="box-art">
+	<div class="text-art">
+    	<span class="text-art-img1"><img src="images/pic03.png"/></span>
+        <div class="text-art-text1">
+        	<h4>钟表</h4>
+            <p>描述描述描述描述描述描述描述描述</p>
+        </div>
+    </div>
+    <div class="text-art">
+    	<span class="text-art-img2"><img src="images/pic04.png"/></span>
+        <div class="text-art-text2">
+        	<h4>贵金属</h4>
+            <p>描述描述描述描述描述描述描述描述</p>
+        </div>
+    </div>
+    <div class="text-art">
+    	<span class="text-art-img3"><img src="images/pic05.png"/></span>
+        <div class="text-art-text3">
+        	<h4>景泰蓝</h4>
+            <p>描述描述描述描述描述描述描述描述</p>
+        </div>
+    </div>
+</div>
+
+<#include "/include/foot.ftl">
--- wishdom/src/com/elink/action/mainAction.java
+++ wishdom/src/com/elink/action/mainAction.java
@@ -11,5 +11,30 @@ public class mainAction extends ActionSupport{
 		JdbcTemplate jdbctemplate = dbConUtil.getCon();
 		return "success";
 	}
+	public String cominfo(){
+		JdbcTemplate jdbctemplate = dbConUtil.getCon();
+		System.out.println("start..........");
+		return "success";
+	}
+	public String pdshow(){
+		JdbcTemplate jdbctemplate = dbConUtil.getCon();
+		System.out.println("start..........");
+		return "success";
+	}
+	public String chzDashi(){
+		JdbcTemplate jdbctemplate = dbConUtil.getCon();
+		System.out.println("start..........");
+		return "success";
+	}
+	public String custMode(){
+		JdbcTemplate jdbctemplate = dbConUtil.getCon();
+		System.out.println("start..........");
+		return "success";
+	}
+	public String zzgy(){
+		JdbcTemplate jdbctemplate = dbConUtil.getCon();
+		System.out.println("start..........");
+		return "success";
+	}
 	
 }
