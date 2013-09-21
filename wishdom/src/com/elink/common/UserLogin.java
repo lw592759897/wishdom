ab139e12e1ea152acac2321075a16072a83d9342
liujp2013 <liujianping@elinkchina.net>
2013-09-21 13:09:04 +0800

    刘建平 提交

---
+++ wishdom/src/com/elink/common/UserLogin.java
@@ -0,0 +1,26 @@
+package com.elink.common;
+
+public class UserLogin {
+
+	private String userLoginId=null;
+	private String userName=null;
+	private String userTye=null; 
+	public String getUserLoginId() {
+		return userLoginId;
+	}
+	public void setUserLoginId(String userLoginId) {
+		this.userLoginId = userLoginId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserTye() {
+		return userTye;
+	}
+	public void setUserTye(String userTye) {
+		this.userTye = userTye;
+	}
+}
